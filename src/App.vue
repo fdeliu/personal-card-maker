15005d2f40f2af21f3fb56f6c6b87a756d8e921b
Flamur Deliu <f.deliu@sitel.com.al>
2020-06-23 18:01:24 +0200

    Initial Commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,81 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <Header />
+    <div class="container">
+      <div class="row">
+        <div class="col-md-5">test</div>
+        <div class="col-md-7"><div class="card-container">
+        <div class="left-side">
+          <a href="#">Baustellenbedarf</a>
+          <p>Hello World</p>
+          <p>Hsfdsf</p>
+        </div>
+        <div class="break-line"></div>
+        <div class="right-side">sdfsdddf</div>
+      </div></div>
+      </div>
+      
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Header from "@/components/Header"
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components:{
+    Header
+  },
+  data: () => ({
+  
+  })
+};
 </script>
+<style scoped>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.card-container {
+  width: 550px;
+  display: flex;
+  border: 2px solid rgb(14, 33, 63);
+}
+a {
+  color: white;
+}
+.left-side {
+  position: relative;
+  width: 300px;
+  height: 220px;
+  padding: 10px 20px 10px 10px;
+  font-size: 20px;
+  position: relative;
+  color: #fff;
+  background: rgb(14, 33, 63);
+}
+.left-side:after {
+  content: " ";
+  position: absolute;
+  display: block;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  z-index: -1;
+  background: rgb(14, 33, 63);
+  transform-origin: bottom left;
+  -ms-transform: skew(-30deg, 0deg);
+  -webkit-transform: skew(-30deg, 0deg);
+  transform: skew(-30deg, 0deg);
+}
+.break-line {
+  width: 10px;
+  height: 220px;
+  margin-left: 66px;
+  transform: skew(-30deg, 0deg);
+  background: #1565c0;
+}
+.right-side{
+  display: flex;
+  margin-left: 70px;
+  align-items: center;
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+    <nav class="navbar navbar-dark bg-primary">
+        <div class="container">
+      <h3 class="text-white">Personal Card Maker</h3>
+        </div>
+    </nav>
+</template>
+
+<script>
+export default {
+    name:"Header"
+}
+</script>
+
+<style scoped>
+.navbar{
+    margin-bottom: 20px;
+    background: rgb(14, 33, 63) !important;
+}
+</style>
