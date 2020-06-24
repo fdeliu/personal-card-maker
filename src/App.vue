5f21910157b7cb8dcd34df208bf44f653bb7aa13
Flamur Deliu <f.deliu@sitel.com.al>
2020-06-24 13:09:34 +0200

    last commit

--- src/App.vue
+++ src/App.vue
@@ -3,36 +3,151 @@
     <Header />
     <div class="container">
       <div class="row">
-        <div class="col-md-5">test</div>
-        <div class="col-md-7"><div class="card-container">
-        <div class="left-side">
-          <a href="#">Baustellenbedarf</a>
-          <p>Hello World</p>
-          <p>Hsfdsf</p>
+        <div class="col-md-5">
+          <h3>Enter your personal data</h3>
+          <form>
+            <div class="form-group">
+              <label for="fullname">Full Name:</label>
+              <input
+                type="name"
+                class="form-control"
+                id="fullname"
+                placeholder="Enter full name"
+                v-model="name"
+              />
+            </div>
+            <div class="form-group">
+              <label for="proffesion">Proffesion:</label>
+              <input
+                type="text"
+                class="form-control"
+                id="proffesion"
+                placeholder="Enter proffesion"
+                v-model="proffesion"
+              />
+            </div>
+            <div class="form-group">
+              <label for="phone">Phone:</label>
+              <input
+                type="text"
+                class="form-control"
+                id="phone"
+                placeholder="Enter phone"
+                v-model="phone"
+              />
+            </div>
+            <div class="form-group">
+              <label for="email">Email:</label>
+              <input
+                type="text"
+                class="form-control"
+                id="email"
+                placeholder="Enter email"
+                v-model="email"
+              />
+            </div>
+            <div class="form-group">
+              <label for="linkedin">Linkedin:</label>
+              <input
+                type="text"
+                class="form-control"
+                id="linkedin"
+                placeholder="Enter linkedin"
+                v-model="linkedin"
+              />
+            </div>
+            <label for="linkedin">Upload Image:</label>
+            <div class="input-group mb-3">
+              <div class="custom-file">
+                <input type="file" class="custom-file-input" id="profile" @change="onFileSelected" />
+                <label class="custom-file-label" for="profile">{{fileName}}</label>
+              </div>
+            </div>
+          </form>
         </div>
-        <div class="break-line"></div>
-        <div class="right-side">sdfsdddf</div>
-      </div></div>
+        <div class="col-md-7">
+          <div class="card-container" id="my-card">
+            <div class="left-side">
+              <h3>{{name}}</h3>
+              <span>{{proffesion}}</span>
+              <div class="contact">
+                <p>
+                  <i class="fa fa-phone-square mr-2" style="font-size:21px"></i>
+                  {{phone}}
+                </p>
+                <p>
+                  <i class="fa fa-envelope mr-2"></i>
+                  {{email}}
+                </p>
+                <p>
+                  <i class="fa fa-linkedin-square mr-2" style="font-size:21px"></i>
+                  {{linkedin}}
+                </p>
+              </div>
+            </div>
+            <div class="break-line"></div>
+            <div class="right-side">
+              <img :src="selectedFile" width="100" height="100" />
+            </div>
+          </div>
+          <button type="button" class="btn btn-success mt-4" @click="download">Download Card</button>
+          <h4 class="text-center mt-3" v-if="isDownloading">Downloading card...</h4>
+        </div>
       </div>
-      
     </div>
   </div>
 </template>
 
 <script>
-import Header from "@/components/Header"
+import Header from "@/components/Header";
+import htmlToImage from "html-to-image";
+import download from "downloadjs";
+import defaultImage from "./assets/john.jpg";
+
 export default {
   name: "App",
-  components:{
+  components: {
     Header
   },
   data: () => ({
-  
-  })
+    name: "Joh Doe",
+    proffesion: "Software Developer",
+    phone: "+35568123456",
+    email: "johndoe@gmail.com",
+    linkedin: "Joh Doe",
+    selectedFile: defaultImage,
+    fileName: "Choose Image",
+    isDownloading: false
+  }),
+  methods: {
+    download() {
+      this.isDownloading = true;
+      htmlToImage
+        .toPng(document.getElementById("my-card"))
+        .then(dataUrl => {
+          this.isDownloading = false;
+          download(dataUrl, "my-card.png");
+        })
+        .catch(err => {
+          this.isDownloading = false;
+          console.log(err);
+        });
+    },
+    onFileSelected(e) {
+      if (!e.target.files[0]) {
+        return;
+      }
+      if (!e.target.files[0].name.match(/.(jpg|jpeg|png)$/i)) {
+        alert("File is not an image! Please choose an image.");
+        return;
+      }
+      this.selectedFile = URL.createObjectURL(e.target.files[0]);
+      this.fileName = e.target.files[0].name;
+    }
+  }
 };
 </script>
 <style scoped>
-
 .card-container {
   width: 550px;
   display: flex;
@@ -40,6 +155,9 @@ export default {
 }
 a {
   color: white;
+}
+p {
+  margin: 0;
 }
 .left-side {
   position: relative;
@@ -73,9 +191,27 @@ a {
   transform: skew(-30deg, 0deg);
   background: #1565c0;
 }
-.right-side{
+.right-side {
   display: flex;
-  margin-left: 70px;
+  margin-left: 50px;
   align-items: center;
+}
+.right-side img {
+  border-radius: 50%;
+}
+form {
+  width: 80%;
+}
+.left-side span {
+  font-size: 18px;
+  color: #ddd1d1;
+}
+.left-side .contact {
+  margin-top: 22px;
+}
+.left-side .contact p {
+  display: flex;
+  align-items: center;
+  font-size: 18px;
 }
 </style>
